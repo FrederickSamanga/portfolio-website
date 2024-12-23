7225ec96ee18c93eb152488b272a9f9ca81795ad
Frederick Samanga <fredericktsamanga@gmail.com>
2024-12-23 01:44:18 -0500

    Started on Hero Section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+             <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I am Frederick</h1>
+             <p className="text-gray-500 text-lg lg:text-xl">
+                I am Java Developer with 5 years of experience.
+                I am also a full stack developer with experience in React, Node.js, and Express.js.
+            </p>
+        </section>
+    );
+};
+
+export default HeroSection;
